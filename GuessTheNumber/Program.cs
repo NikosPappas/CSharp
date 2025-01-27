9e9284738f536109ab71e32e73a19a47c851f2ba
NikosPappas <nikolaosgpappast@gmail.com>
2025-01-27 06:27:04 +0200

    Add files via upload
    A terminal based application using Random class  to create a guess the number game.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿// 1. Generate a Random Number
+Random random = new Random();
+int secretNumber = random.Next(1, 101); // Generates a number between 1 and 1
+
+bool guessedCorrectly = false;
+while (!guessedCorrectly)
+{
+    Console.Write("Guess a number between 1 and 100: ");
+    string input = Console.ReadLine();
+    int guess;
+    if (!int.TryParse(input, out guess))
+    {
+        Console.WriteLine("Invalid input. Please enter a number.");
+    // Handle invalid input (ask again or end the game)
+    }
+    else
+    {
+        // 3. Check the Guess (within the loop
+        if (guess < secretNumber)
+        {
+           Console.WriteLine("Too low!");
+        }
+          else if (guess > secretNumber)
+        {
+           Console.WriteLine("Too high!");
+        }
+         else
+        {
+        Console.WriteLine("You guessed it! Congratulations!");
+        }
+    }
+    if(guess == secretNumber)
+    {
+        guessedCorrectly = true;
+    }
+}
